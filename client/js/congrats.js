ce5ac1d3d736952ad411cb7b877c05f8ad0eccf6
e-a-n <ean.director@gmail.com>
2019-01-16 12:46:24 -0800

    added html based congrats, fixed game over bug, added new files.  Preparing to add input event for html to restart game

---
+++ client/js/congrats.js
@@ -0,0 +1,10 @@
+const displayCongrats = () => {
+    const gameContainer = document.getElementById("gameContainer");
+    const gameUI = document.getElementById("myCanvas");
+
+    let congratsText = document.createElement("h2");
+    congratsText.innerHTML = "Congrats! You Won the Game!!";
+
+    gameUI.style.display = "none";
+    gameContainer.appendChild(congratsText);
+};
